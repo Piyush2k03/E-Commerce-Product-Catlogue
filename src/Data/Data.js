0195b4b2304b8d71e065419da0dcec206ded2761
kartikeyk07 <kongekartikey007@gmail.com>
2025-04-16 11:00:55 +0530

    Initial commit

---
+++ src/Data/Data.js
@@ -0,0 +1,766 @@
+export const AllWear = [
+  {
+    id: 1,
+    name: "Men V Neck Pure Cotton Pink T-Shirt",
+    price: 80,
+    image: "1.png",
+    titleImage: "1.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 2,
+    name: "Women Rounded Neck Pure Cotton T-Shirt",
+    price: 82,
+    image: "16.png",
+    titleImage: "16.png",
+    
+    category: "women",
+    type: "topWear",
+  },
+  {
+    id: 3,
+    name: "Black Puma Pure Cotton Shirt",
+    price: 93,
+    image: "2.png",
+    titleImage: "2.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 4,
+    name: "Women Green Short Trousers",
+    price: 49,
+    image: "52.png",
+    titleImage: "52.png",
+    
+    category: "women",
+    type: "topWear",
+  },
+  {
+    id: 5,
+    name: "Women Black Stripe Sleep-Wear Trousers",
+    price: 29,
+    image: "51.png",
+    titleImage: "51.png",
+    
+    category: "trousers",
+    type: "bottomWear",
+  },
+  {
+    id: 6,
+    name: "Boys Plain WHite Pure Cotton Shirt",
+    price: 19,
+    image: "42.png",
+    titleImage: "42.png",
+    
+    category: "kid",
+    type: "topWear",
+  },
+  {
+    id: 7,
+    name: "Puma White Pure Cotton T-Shirt",
+    price: 69,
+    image: "15.png",
+    titleImage: "15.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+
+  {
+    id: 8,
+    name: "Pink Rounded Neck Pure Cotton Top",
+    price: 21,
+    image: "34.png",
+    titleImage: "34.png",
+    
+    category: "kid",
+    type: "topWear",
+  },
+  {
+    id: 9,
+    name: "Boys Plain Blue  Cotton Jacket",
+    price: 38,
+    image: "36.png",
+    titleImage: "36.png",
+    
+    category: "kid",
+    type: "topWear",
+  },
+  {
+    id: 10,
+    name: "Long Sleeve Stripe Pure Cotton Shirt",
+    price: 101,
+    image: "3.png",
+    titleImage: "3.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 11,
+    name: "Boys Plain WHite Pure Cotton Shirt",
+    price: 19,
+    image: "42.png",
+    titleImage: "42.png",
+    
+    category: "kid",
+    type: "topWear",
+  },
+
+  {
+    id: 12,
+    name: "Pink Rounded Neck Pure Cotton Top",
+    price: 21,
+    image: "34.png",
+    titleImage: "34.png",
+    
+    category: "kid",
+    type: "topWear",
+  },
+
+  {
+    id: 13,
+    name: "Dark Blue Puffy Rounded Jacket",
+    price: 120,
+    image: "8.png",
+    titleImage: "8.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 14,
+    name: "Black Tapered Fit Flat-Front Trousers",
+    price: 41,
+    image: "46.png",
+    titleImage: "46.png",
+    
+    category: "trousers",
+    type: "bottomWear",
+  },
+  {
+    id: 15,
+    name: "Girls Rounded Neck Pure Cotton T-Shirt",
+    price: 16,
+    image: "35.png",
+    titleImage: "35.png",
+    
+    category: "kids",
+    type: "topWear",
+  },
+  {
+    id: 16,
+    name: "Men Puffy V Neck Pure Jacket",
+    price: 99,
+    image: "6.png",
+    titleImage: "6.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 17,
+    name: "Men Stripes Pure Cotton T-Shirt",
+    price: 100,
+    image: "7.png",
+    titleImage: "7.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+
+  {
+    id: 18,
+    name: "Dark Blue Rounded Neck Pure Cotton Jacket",
+    price: 95,
+    image: "9.png",
+    titleImage: "9.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 19,
+    name: "Women Black Stripe Sleep-Wear Trousers",
+    price: 29,
+    image: "51.png",
+    titleImage: "51.png",
+    
+    category: "trousers",
+    type: "bottomWear",
+  },
+  {
+    id: 20,
+    name: "Women Green Short Trousers",
+    price: 49,
+    image: "52.png",
+    titleImage: "52.png",
+    
+    category: "trousers",
+    type: "bottomWear",
+  },
+  {
+    id: 21,
+    name: "Dark Blue Rounded Open Jacket",
+    price: 77,
+    image: "10.png",
+    titleImage: "10.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 22,
+    name: "Light Blue Rounded Neck Pure Cotton T-Shirt",
+    price: 88,
+    image: "11.png",
+    titleImage: "11.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 23,
+    name: "Armless Black Pure Cotton T-Shirt",
+    price: 61,
+    image: "12.png",
+    titleImage: "12.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 24,
+    name: "Black Short Sleeve Pure Cotton T-Shirt",
+    price: 49,
+    image: "13.png",
+    titleImage: "13.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 25,
+    name: "Puma White Pure Cotton T-Shirt",
+    price: 69,
+    image: "15.png",
+    titleImage: "15.png",
+    
+    category: "men",
+    type: "topWear",
+  },
+  {
+    id: 26,
+    name: "Plain Rounded Neck Pure Cotton T-Shirt",
+    price: 64,
+    image: "23.png",
+    titleImage: "23.png",
+    
+    category: "women",
+    type: "topWear",
+  },
+  {
+    id: 27,
+    name: "Women Winter Pink Cotton Jacket",
+    price: 82,
+    image: "24.png",
+    titleImage: "24.png",
+    
+    category: "women",
+    type: "topWear",
+  },
+  {
+    id: 28,
+    name: "Women Black Stripe Sleep-Wear Trousers",
+    price: 29,
+    image: "51.png",
+    titleImage: "51.png",
+    
+    category: "trousers",
+    type: "topWear",
+  },
+  {
+    id: 29,
+    name: "Black Tapered Fit Flat-Front Trousers",
+    price: 41,
+    image: "46.png",
+    titleImage: "46.png",
+    
+    category: "trousers",
+    type: "bottomWear",
+  },
+];
+
+export const menWear = [
+  {
+    name: "Men V Neck Pure Cotton Pink T-Shirt",
+    price: 80,
+    image: "1.png",
+    titleImage: "1.png",
+    
+    category: "men",
+  },
+  {
+    name: "Black Puma Pure Cotton Shirt",
+    price: 93,
+    image: "2.png",
+    titleImage: "2.png",
+    
+    category: "men",
+  },
+  {
+    name: "Long Sleeve Stripe Pure Cotton Shirt",
+    price: 101,
+    image: "3.png",
+    titleImage: "3.png",
+    
+    category: "men",
+  },
+  {
+    name: "Short Sleeve Pure Cotton Top ",
+    price: 92,
+    image: "4.png",
+    titleImage: "4.png",
+    
+    category: "men",
+  },
+  {
+    name: "Plain White Rounded Neck Top",
+    price: 83,
+    image: "5.png",
+    titleImage: "5.png",
+    
+    category: "men",
+  },
+  {
+    name: "Men Puffy V Neck Pure Jacket",
+    price: 99,
+    image: "6.png",
+    titleImage: "6.png",
+    
+    category: "men",
+  },
+  {
+    name: "Men Stripes Pure Cotton T-Shirt",
+    price: 100,
+    image: "7.png",
+    titleImage: "7.png",
+    
+    category: "men",
+  },
+  {
+    name: "Dark Blue Puffy Rounded Jacket",
+    price: 120,
+    image: "8.png",
+    titleImage: "8.png",
+    
+    category: "men",
+  },
+  {
+    name: "Plain Rounded Neck Pure Cotton T-Shirt",
+    price: 64,
+    image: "23.png",
+    titleImage: "23.png",
+    
+    category: "women",
+  },
+  {
+    name: "Women Winter Pink Cotton Jacket",
+    price: 82,
+    image: "24.png",
+    titleImage: "24.png",
+    
+    category: "women",
+  },
+  {
+    name: "Dark Blue Rounded Neck Pure Cotton Jacket",
+    price: 95,
+    image: "9.png",
+    titleImage: "9.png",
+    
+    category: "men",
+  },
+  {
+    name: "Dark Blue Rounded Open Jacket",
+    price: 77,
+    image: "10.png",
+    titleImage: "10.png",
+    
+    category: "men",
+  },
+  {
+    name: "Light Blue Rounded Neck Pure Cotton T-Shirt",
+    price: 88,
+    image: "11.png",
+    titleImage: "11.png",
+    
+    category: "men",
+  },
+  {
+    name: "Armless Black Pure Cotton T-Shirt",
+    price: 61,
+    image: "12.png",
+    titleImage: "12.png",
+    
+    category: "men",
+  },
+  {
+    name: "Black Short Sleeve Pure Cotton T-Shirt",
+    price: 49,
+    image: "13.png",
+    titleImage: "13.png",
+    
+    category: "men",
+  },
+  {
+    name: "Puma White Pure Cotton T-Shirt",
+    price: 69,
+    image: "15.png",
+    titleImage: "15.png",
+    
+    category: "men",
+  },
+];
+
+export const womenWear = [
+  {
+    name: "Women Rounded Neck Pure Cotton T-Shirt",
+    price: 82,
+    image: "16.png",
+    titleImage: "16.png",
+    
+    category: "women",
+  },
+  {
+    name: "Women Puma Cotton T-Shirt",
+    price: 81,
+    image: "17.png",
+    titleImage: "17.png",
+    
+    category: "women",
+  },
+  {
+    name: "Light Blue V Neck Pure Cotton T-Shirt",
+    price: 64,
+    image: "18.png",
+    titleImage: "18.png",
+    
+    category: "women",
+  },
+  {
+    name: "Wine Rounded Neck Pure Cotton Jacket",
+    price: 82,
+    image: "19.png",
+    titleImage: "19.png",
+    
+    category: "women",
+  },
+  {
+    name: "Women Puma Cotton T-Shirt",
+    price: 76,
+    image: "20.png",
+    titleImage: "20.png",
+    
+    category: "women",
+  },
+  {
+    name: "Jean Jacket Pure Cotton",
+    price: 82,
+    image: "21.png",
+    titleImage: "21.png",
+    
+    category: "women",
+  },
+  {
+    name: "Women Nike Pure Cotton Tracksuit",
+    price: 81,
+    image: "22.png",
+    titleImage: "22.png",
+    
+    category: "women",
+  },
+  {
+    name: "Plain Rounded Neck Pure Cotton T-Shirt",
+    price: 64,
+    image: "23.png",
+    titleImage: "23.png",
+    
+    category: "women",
+  },
+  {
+    name: "Women Winter Pink Cotton Jacket",
+    price: 82,
+    image: "24.png",
+    titleImage: "24.png",
+    
+    category: "women",
+  },
+  {
+    name: "Women Rounded Neck Pure Cotton T-Shirt",
+    price: 76,
+    image: "25.png",
+    titleImage: "25.png",
+    
+    category: "women",
+  },
+  {
+    name: "Pink Rounded Neck Pure Cotton Jacket",
+    price: 42,
+    image: "26.png",
+    titleImage: "26.png",
+    
+    category: "women",
+  },
+  {
+    name: "Women Rounded Neck Pure Cotton T-Shirt",
+    price: 70,
+    image: "27.png",
+    titleImage: "27.png",
+    
+    category: "women",
+  },
+];
+
+export const kidsWear = [
+  {
+    name: "Girls Rounded Neck Pure Cotton Blouse",
+    price: 32,
+    image: "28.png",
+    titleImage: "28.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Boys Plain Blue  Cotton Jacket",
+    price: 38,
+    image: "36.png",
+    titleImage: "36.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Pink Rounded Neck Pure Cotton Top",
+    price: 29,
+    image: "29.png",
+    titleImage: "29.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Plain Rounded Neck White Cotton T-Shirt",
+    price: 38,
+    image: "37.png",
+    titleImage: "37.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Light Blue Rounded Neck Pure Cotton T-Shirt",
+    price: 32,
+    image: "30.png",
+    titleImage: "30.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Black Rounded Neck Pure Cotton T-Shirt",
+    price: 27,
+    image: "38.png",
+    titleImage: "38.png",
+  },
+  {
+    name: "Boys Armless Red Pure Cotton Sportswear",
+    price: 19,
+    image: "39.png",
+    titleImage: "39.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Girls Rounded Neck White Top Shirt",
+    price: 62,
+    image: "31.png",
+    titleImage: "31.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Boys V Neck Pure Stripes Cotton T-Shirt",
+    price: 19,
+    image: "40.png",
+    titleImage: "40.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Girls Pinks Neck Pure Cotton Wear",
+    price: 22,
+    image: "32.png",
+    titleImage: "32.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Boys Rounded Neck Pure Cotton T-Shirt",
+    price: 19,
+    image: "41.png",
+    titleImage: "41.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Girls Rounded Neck Pure Cotton Shirt",
+    price: 22,
+    image: "33.png",
+    titleImage: "33.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Boys Plain WHite Pure Cotton Shirt",
+    price: 19,
+    image: "42.png",
+    titleImage: "42.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Pink Rounded Neck Pure Cotton Top",
+    price: 21,
+    image: "34.png",
+    titleImage: "34.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Girls Rounded Neck Pure Cotton T-Shirt",
+    price: 16,
+    image: "35.png",
+    titleImage: "35.png",
+    
+    category: "kid",
+  },
+];
+
+export const trousers = [
+  {
+    name: "Men Tapered Fit Flat-Front Trousers",
+    price: 27,
+    image: "43.png",
+    titleImage: "43.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Grey Sport Fit Track Trousers",
+    price: 16,
+    image: "44.png",
+    titleImage: "44.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Men Blue Fit Jean Trousers",
+    price: 19,
+    image: "45.png",
+    titleImage: "45.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Black Tapered Fit Flat-Front Trousers",
+    price: 41,
+    image: "46.png",
+    titleImage: "46.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Women Tapered Black-White Trousers",
+    price: 16,
+    image: "47.png",
+    titleImage: "47.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Blue Baggy Jean Trousers",
+    price: 22,
+    image: "48.png",
+    titleImage: "48.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Light Blue Tapered Fit Trousers",
+    price: 31,
+    image: "49.png",
+    titleImage: "49.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Women Pink Stripe Sleep-Wear Trousers",
+    price: 17,
+    image: "50.png",
+    titleImage: "50.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Women Black Stripe Sleep-Wear Trousers",
+    price: 29,
+    image: "51.png",
+    titleImage: "51.png",
+    
+    category: "trousers",
+  },
+  {
+    name: "Women Green Short Trousers",
+    price: 49,
+    image: "52.png",
+    titleImage: "52.png",
+    
+    category: "trousers",
+  },
+];
+
+export const bestSellers = [
+  {
+    name: "Jean Jacket Pure Cotton",
+    price: 82,
+    image: "21.png",
+    titleImage: "21.png",
+    
+    category: "women",
+  },
+  {
+    name: "Men Stripes Pure Cotton T-Shirt",
+    price: 100,
+    image: "7.png",
+    titleImage: "7.png",
+    
+    category: "men",
+  },
+
+  {
+    name: "Boys Armless Red Pure Cotton Sportswear",
+    price: 19,
+    image: "39.png",
+    titleImage: "39.png",
+    
+    category: "kid",
+  },
+  {
+    name: "Girls Rounded Neck White Top Shirt",
+    price: 62,
+    image: "31.png",
+    titleImage: "31.png",
+    
+    category: "kid",
+  },
+];
